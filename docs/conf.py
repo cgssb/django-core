d4374254b3aa5e1b8cb70a3df1ecfbb5533a6030
Brian Koebbe <brian@koebbe.org>
2014-08-19 22:29:45 -0500

    trying to get docs to build without error

--- docs/conf.py
+++ docs/conf.py
@@ -23,8 +23,8 @@ import os
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 #sys.path.insert(0, os.path.abspath('../core'))
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-#from django.conf import settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+from django.conf import settings
 
 #sys.path.insert(0, os.path.join(this, os.pardir))
 
